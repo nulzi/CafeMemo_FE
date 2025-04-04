c784f3fb5c89d523640d73170d59fdd618bfe443
nulzi <kimty131@naver.com>
2025-04-04 15:19:56 +0900

    Add: 기본 테이블 정보 consts 폴더로 분리

---
+++ src/consts/index.ts
@@ -0,0 +1,67 @@
+export const DEFAULT_TABLE = [
+  {
+    tableId: "table-0",
+    tableName: "1",
+    teams: [],
+  },
+  {
+    tableId: "table-1",
+    tableName: "2",
+    teams: [],
+  },
+  {
+    tableId: "table-2",
+    tableName: "3",
+    teams: [],
+  },
+  {
+    tableId: "table-3",
+    tableName: "4",
+    teams: [],
+  },
+  {
+    tableId: "table-4",
+    tableName: "4.5",
+    teams: [],
+  },
+  {
+    tableId: "table-5",
+    tableName: "5",
+    teams: [],
+  },
+  {
+    tableId: "table-6",
+    tableName: "6",
+    teams: [],
+  },
+  {
+    tableId: "table-7",
+    tableName: "7",
+    teams: [],
+  },
+  {
+    tableId: "table-8",
+    tableName: "8",
+    teams: [],
+  },
+  {
+    tableId: "table-9",
+    tableName: "파티룸",
+    teams: [],
+  },
+  {
+    tableId: "table-10",
+    tableName: "핑1",
+    teams: [],
+  },
+  {
+    tableId: "table-11",
+    tableName: "핑2",
+    teams: [],
+  },
+  {
+    tableId: "table-12",
+    tableName: "핑3",
+    teams: [],
+  },
+];
